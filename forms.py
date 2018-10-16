1b51e8852eb3b5ef0c6018fc860c3d92c5e28ba8
Khoi <hakhoipham@gmail.com>
2018-10-16 14:20:19 -0400

    added database, login and register routes, form models

---
+++ forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+# also came along with flask_wtf
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    # need validators to ensure data is proper
+    # variable = Type of field(label_name, array of validation checks)
+    username = StringField("Username",
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField("Confirm Password",
+                                     validators=[DataRequired(), EqualTo("password")])
+    submit = SubmitField("Sign Up")
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Login")
