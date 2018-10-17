0fdb7913acf8140c4def6a58a65f4b1ab41d1af3
Khoi <hakhoipham@gmail.com>
2018-10-17 11:53:52 -0400

    added edit and delete posts functionalities

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -3,7 +3,7 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 # also came along with flask_wtf
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 
@@ -61,3 +61,9 @@ class UpdateAccountForm(FlaskForm):
             if user:
                 raise ValidationError(
                     "That email is taken. Please choose a different one.")
+
+
+class PostForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    content = TextAreaField("Content", validators=[DataRequired()])
+    submit = SubmitField("Post")
